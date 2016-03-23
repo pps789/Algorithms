ea52fcb338f093bdb746abe54c7ce37e966190a5
Sanghyeok Park <pps0209@gmail.com>
2016-03-23 17:12:19 +0900

    LCP

---
+++ string/lcp.cpp
@@ -0,0 +1,31 @@
+#include<vector>
+using namespace std;
+
+template <typename Ty>
+class LCP{
+public:
+    const vector<int> &sa;
+    const vector<Ty> &str;
+    vector<int> lcp;
+
+    LCP(const vector<Ty> &_str, const vector<int> &_sa): str(_str), sa(_sa) {}
+
+    void process(){
+        int n = sa.size();
+        lcp.resize(n);
+        vector<int> sa_inv(n);
+
+        for(int i = 0; i < n; i++)
+            sa_inv[sa[i]] = i;
+
+        int l = 0;
+        for(int i = 0; i < n; i++){
+            if(sa_inv[i] > 0){
+                int j = sa[sa_inv[i] - 1];
+                while(i + l < n && j + l < n && str[i + l] == str[j + l]) l++;
+                lcp[sa_inv[i]] = l;
+                if(l > 0) l--;
+            }
+        }
+    }
+};
